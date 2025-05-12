64d5ad73c2b32213f6bb3b3c25b55453c71c9e4c
Thiago <115191028+thiago-deones@users.noreply.github.com>
2025-05-12 16:32:31 -0300

    Refactor IMC calculation logic to improve readability and maintainability

--- JavaScript/calculadoraIMC/script/imc.js
+++ JavaScript/calculadoraIMC/script/imc.js
@@ -2,12 +2,33 @@ const calcular = document.getElementById("calcular");
 
 function imc() {
     const nome = document.getElementById("nome").value;
-    const altura = document.getElementById("altura").value;
-    const peso = document.getElementById("peso").value;
+    let altura = document.getElementById("altura").value;
+    let peso = document.getElementById("peso").value;
     const resultado = document.getElementById("resultado");
 
     if (nome !== '' && altura !== '' && peso !== '') {
+        altura = parseFloat(altura.replace(',', '.'));
+        peso = parseFloat(peso.replace(',', '.'));
+
         const  valorIMC = (peso / (altura * altura)).toFixed(2);
+
+        let classificacao = '';
+        if (valorIMC < 18.5) {
+            classificacao = 'Abaixo do peso';
+        } else if (valorIMC < 25) {
+            classificacao = 'Peso normal';
+        } else if (valorIMC < 30) {
+            classificacao = 'Sobrepeso';
+        } else if (valorIMC < 35) {
+            classificacao = 'Obesidade grau 1';
+        } else if (valorIMC < 40) {
+            classificacao = 'Obesidade grau 2';
+        } else {
+            classificacao = 'Obesidade grau 3';
+        }
+
+        resultado.textContent = `Olá ${nome}, seu IMC é ${valorIMC} e você está ${classificacao}.`;
+
     } else {
         resultado.textContent = 'Preencha todos os campos!!!';
    }
