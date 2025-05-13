4bd0caf346b0b78bf030ecaffb826af77a0525d0
Thiago <115191028+thiago-deones@users.noreply.github.com>
2025-05-13 19:35:55 -0300

    refactor: improve traffic light functionality and styling

--- JavaScript/semafaro/semaforo.js
+++ JavaScript/semafaro/semaforo.js
@@ -1,10 +1,11 @@
 const img = document.getElementById( 'img' );
 const buttons = document.getElementById( 'buttons' );
 let colorIndex = 0;
+let interval = null;
 
 const trafficLight = ( event ) => {
+    stopAutomatic();
     turnOn[event.target.id]();
-    
 }
 
 const nextIndex = () => colorIndex = colorIndex < 2 ? ++colorIndex : 0;
@@ -17,11 +18,15 @@ const changeColor = () => {
     nextIndex();
 }
 
+const stopAutomatic = () => {
+    clearInterval( interval )
+}
+
 const turnOn = {
     'red':      () => img.src = "./img/vermelho.png",
     'yellow':   () => img.src = "./img/amarelo.png",
     'green':    () => img.src = "./img/verde.png",
-    'automatic':() => setInterval(changeColor, 1000)
+    'automatic':() => interval = setInterval(changeColor, 1000)
 }
 
 buttons.addEventListener( 'click', trafficLight);
