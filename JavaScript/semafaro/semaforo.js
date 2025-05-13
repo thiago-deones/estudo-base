1e2002b10830abc59763509da09bdab7c30a2a08
Thiago <115191028+thiago-deones@users.noreply.github.com>
2025-05-13 19:15:00 -0300

    feat: add traffic light simulation with HTML, CSS, and JavaScript
    - Created index.html for the traffic light interface with buttons for each color and an automatic mode.
    - Implemented semaforo.js to handle traffic light logic, including color changes and automatic cycling.
    - Styled the application with style.css to ensure a responsive and centered layout.

---
+++ JavaScript/semafaro/semaforo.js
@@ -0,0 +1,29 @@
+const img = document.getElementById( 'img' );
+const buttons = document.getElementById( 'buttons' );
+let colorIndex = 0;
+
+const trafficLight = ( event ) => {
+    turnOn[event.target.id]();
+    
+}
+
+const nextIndex = () => {
+    
+    colorIndex = colorIndex < 2 ? ++colorIndex : 0;
+}
+
+const changeColor = () => {
+    const colores = ['red', 'yellow', 'green'];
+    const color = colores[ colorIndex ];
+    turnOn[color]();
+    nextIndex();
+}
+
+const turnOn = {
+    'red':      () => img.src = "./img/vermelho.png",
+    'yellow':   () => img.src = "./img/amarelo.png",
+    'green':    () => img.src = "./img/verde.png",
+    'automatic':() => setInterval(changeColor, 1000)
+}
+
+buttons.addEventListener( 'click', trafficLight);
