3717e77a07083ba773ee852b3f2bfa7d741be0b3
Thiago <115191028+thiago-deones@users.noreply.github.com>
2025-05-16 16:56:34 -0300

    refactor: add idade comparison function for pessoa instances

--- JavaScript/aula007/aula.js
+++ JavaScript/aula007/aula.js
@@ -16,3 +16,13 @@ const lucas = new pessoa();
 lucas.nome = "lucas";
 lucas.idade = 23;
 lucas.descrever();
+
+function compararIdade(p1, p2) {
+    if (p1.idade > p2.idade) {
+        console.log(`${p1.nome} é mais velho(a) que ${p2.nome}`)
+    } else if (p1.idade < p2.idade) {
+        console.log(`${p2.nome} é mais velho(a) que ${p1.idade}`)
+    } else {
+        console.log(`${p1.nome} e ${p2.nome} tem a mesma idade`)
+    }
+}
