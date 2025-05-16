0e1d5b2591b96fe2e9f70a89a41c26b76791235a
Thiago <115191028+thiago-deones@users.noreply.github.com>
2025-05-16 17:42:04 -0300

    feat: initialize alunos array with basic array operations

---
+++ JavaScript/aula008/aula.js
@@ -0,0 +1,15 @@
+const alunos = ['João', 'Maria', 'José', 'Ana', 'Pedro'];
+
+alunos.push('Lucas');//adicionando no final
+
+alunos.unshift('Carlos');//adicionando no começo
+
+//alunos.pop();//removendo do final
+
+alunos.shift();//removendo do começo
+
+//alunos.splice(2, 1);//removendo do meio
+
+//alunos.splice(2, 0, 'Lucas');//adicionando no meio
+//alunos.splice(2, 1, 'Lucas');//substituindo do meio
+console.log(alunos);
