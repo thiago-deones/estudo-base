46d5f3d1cfb3d7998a7f2a0e6031d6aefb2de2f4
Thiago <115191028+thiago-deones@users.noreply.github.com>
2025-05-17 16:49:37 -0300

    refactor: replace alunos array with notas array and implement average calculation

--- JavaScript/aula008/aula.js
+++ JavaScript/aula008/aula.js
@@ -24,5 +24,22 @@ notas.push(5);
 notas.push(7);
 notas.push(8);
 
+let soma = 0;
+
+for (let i = 0; i < notas.length; i++) {
+    soma += notas[i];
+}
+
+console.log(soma);
+
+console.log('A média é: ' + (soma/notas.length));
 
 
+
+const nome = 'Thiago';
+
+for (let i = 0; i < nome.length; i++) {
+
+    console.log(nome[i]);
+
+}
