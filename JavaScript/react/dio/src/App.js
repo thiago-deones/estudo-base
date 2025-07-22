c6b0743371d38e5f7b83c55659767b7eee57b2e1
Thiago <115191028+thiago-deones@users.noreply.github.com>
2025-07-22 15:47:23 -0300

    feat: initialize React application with basic structure and components
    - Created package.json with dependencies and scripts for React app
    - Added index.html for the main HTML structure
    - Implemented App component with a greeting and a Button component
    - Created Button component to render a button with a title prop
    - Set up index.js to render the App component into the root div

---
+++ JavaScript/react/dio/src/App.js
@@ -0,0 +1,12 @@
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá React</h1>
+      <Button title= "Abrir"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ JavaScript/react/dio/src/components/Button/index.js
@@ -0,0 +1,9 @@
+export default function Button(props) {
+    const { title } = props;
+  return (
+    <button>
+        {title}
+    </button>
+  )
+}
+
